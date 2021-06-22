b6faef8c9340f8dddaa5a73c93ff505d94d7d8c0
kidileAsh <84465351+kidileAsh@users.noreply.github.com>
2021-06-22 21:36:10 +0530

    my sellcar website

---
+++ assets/js/style.js
@@ -0,0 +1,32 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+// Popular 
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  }) 
+//Vehcles
+
+$('#blogCarousel').carousel({
+  interval: 3000
+});  
